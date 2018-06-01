99911f6364e8eef62e06b49430af5879ac215a36
bcaneba <bcaneba@gmail.com>
2018-05-31 20:00:17 -0500

    added to index page

---
+++ spec/systems/sport_show_system_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "the user clicks on a sport to display the show", :type => :feature do
+
+  # before :each do
+  # 	@venue = FactoryBot.create(:venue)
+  #   @event = FactoryBot.create(:event, venue: @venue)
+  # end
+
+ #  it "brings the user to the event show page" do
+ #  	visit root_path
+ #    first('.event-title').click
+ #    expect(page).to have_content @event.title
+	# end
+
+  
+end
